02d01f0973a6c5e5cbe2dd94c5260b742c5d1d46
Franky Cunza Chavez <franky.cunza@gmail.com>
2025-09-03 05:50:02 -0500

    aa

--- mobile/init.mjs
+++ mobile/init.mjs
@@ -30,61 +30,6 @@ let advisors = [
         password: '1'
     }
 ]
-
-let clients = [
-    {
-        id: 344,
-        name: "Daniel Suarez",
-        document_number: "88393839",
-        address: "Av, los proceres 123",
-        district: "Lince",
-        balance: 423.0,
-        blackberry: 3.5,
-        promises_to_pay_today: 5,
-        new_disbursements: 120,
-        routes: [],
-        visits: []
-    },
-    {
-        id: 543,
-        name: "Edwin Donaire",
-        document_number: "88393839",
-        address: "Av, los proceres 123",
-        district: "Lince",
-        balance: 538.0,
-        blackberry: 3.5,
-        promises_to_pay_today: 5,
-        new_disbursements: 22,
-        routes: [],
-        visits: []
-    },
-    {
-        id: 533,
-        name: "Ernesto Ortega",
-        document_number: "88393839",
-        address: "Av, los proceres 123",
-        district: "Lince",
-        balance: 723.0,
-        blackberry: 3.5,
-        promises_to_pay_today: 5,
-        new_disbursements: 33,
-        routes: [],
-        visits: []
-    },
-    {
-        id: 884,
-        name: "Juan Soto",
-        document_number: "88393839",
-        address: "Av, los proceres 123",
-        district: "Lince",
-        balance: 234.0,
-        blackberry: 3.5,
-        promises_to_pay_today: 5,
-        new_disbursements: 44,
-        routes: [],
-        visits: []
-    },
-]
 router.post("/login", async (req, res) => {
     console.log("Login");
     const { username, password } = req.body;
@@ -166,28 +111,25 @@ router.post("/routes/add", async (req, res) => {
 
 router.post("/visits/add", async (req, res) => {
     const { client_id, signature, notes, photos } = req.body
-    console.log("Visits")
-    console.log({ client_id, signature, notes, photos })
-    const client = clients.find(el => el.id == client_id)
-    if (client) {
-        console.log("Has client")
-        clients = clients.reduce((acc, cur) => {
-            const data = cur
-            if (cur.id === client_id) {
-                data['visits'] = [
-                    ...data['visits'],
-                    {
-                        date: new Date(),
-                        signature,
-                        notes,
-                        photos
-                    }
-                ]
-            }
-            acc.push(data)
-            return acc
-        }, [])
+    const response = await fetch(`${END_POINT_SUPABASE}/rest/v1/visits`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Prefer: "return=representation",
+            apikey: SUPABASE_API_KEY,
+            Authorization: `Bearer ${SUPABASE_API_KEY}`,
+        },
+        body: JSON.stringify({
+            client_id,
+            signature,
+            notes,
+            photos
+        })
+    });
+    if (!response.ok) {
+        throw new Error(`Error HTTP: ${response.status}`);
     }
+    console.log({ client_id, signature, notes, photos })
     return res.json({
         success: true,
         message: 'Se Agregó correctamente'
