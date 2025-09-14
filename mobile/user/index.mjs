a6ca0514fab1b425d90c0bb24511824b5222d84c
Franky Cunza Chavez <franky.cunza@gmail.com>
2025-09-14 15:07:19 -0500

    last changes

--- mobile/user/index.mjs
+++ mobile/user/index.mjs
@@ -5,22 +5,36 @@ const END_POINT_SUPABASE = "https://srhpcnaonhyzwvirvczi.supabase.co";
 const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyaHBjbmFvbmh5end2aXJ2Y3ppIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Njg2Nzc4NSwiZXhwIjoyMDcyNDQzNzg1fQ.qYVh94hfn-eWPrsPlEBFkueJWwVOlzJZDxHnVZSVHNM";
 
 router.get("/requests", async (req, res) => {
-    const response = await fetch(`${END_POINT_SUPABASE}/rest/v1/requests?select=*`, {
-        method: "GET",
-        headers: {
-            apikey: SUPABASE_API_KEY,
-            Authorization: `Bearer ${SUPABASE_API_KEY}`,
-        },
-    });
+    try {
+        const { user_id } = req.query;
 
-    if (!response.ok) {
-        throw new Error(`Error HTTP: ${response.status}`);
+        const response = await fetch(
+            `${END_POINT_SUPABASE}/rest/v1/requests?select=*&user_id=eq.${user_id}`, 
+            {
+                method: "GET",
+                headers: {
+                    apikey: SUPABASE_API_KEY,
+                    Authorization: `Bearer ${SUPABASE_API_KEY}`,
+                },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return res.json({
+            success: true,
+            data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
     }
-    const data = await response.json();
-    return res.json({
-        success: true,
-        data: data,
-    });
 });
+
 
 export default router;
